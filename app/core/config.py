b8045b0d37995f1aa49583a3941ab9ea3d5c5027
Daniel Caicedo <79131464+danielcaicedo95@users.noreply.github.com>
2025-04-18 09:53:05 -0500

    config claves

--- app/core/config.py
+++ app/core/config.py
@@ -7,3 +7,7 @@ load_dotenv()
 WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
 WHATSAPP_PHONE_NUMBER_ID = os.getenv("WHATSAPP_PHONE_NUMBER_ID")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# Supabase
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
